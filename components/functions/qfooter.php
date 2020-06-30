9a9ac12d0e91d5a6481f6d424fb7785891abe910
amimaro <amir.zahlan@gmail.com>
2020-06-30 12:03:40 -0300

    Remove footer reveal from functions

--- components/functions/qfooter.php
+++ components/functions/qfooter.php
@@ -1,11 +1,6 @@
 <th scope="row">QFooter:</th>
 <td>
   <div>
-    <label for="quasarwp-settings-qfooter-reveal">
-      <input name="quasarwp-settings[qfooter-reveal]" type="checkbox" value="1" <?php checked(isset($options['qfooter-reveal'])); ?> id="quasarwp-settings-qfooter-reveal" />
-      Footer Reveal
-    </label>
-    <br>
     <label for="quasarwp-settings-qfooter-icon">
       <input name="quasarwp-settings[qfooter-icon]" type="checkbox" value="1" <?php checked(isset($options['qfooter-icon'])); ?> id="quasarwp-settings-qfooter-icon" />
       Show Site Icon
