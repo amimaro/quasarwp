d77f5e660c31a151e0385a82d6835f2e69b60ba8
amimaro <amir.zahlan@gmail.com>
2020-07-04 10:23:37 -0300

    Delete load.php

--- functions.php
+++ functions.php
@@ -2,7 +2,17 @@
 
 add_action('admin_menu', 'quasarwp_menu');
 
-include('data/load.php');
+if (!get_option('quasarwp-settings')) {
+  add_option('quasarwp-settings', array(
+    'roboto-font' => 1,
+    'material-icons' => 1,
+    'fontawesomev5' => 1,
+    'minified-files' => 1,
+    'language' => 'en-us',
+    'icon-set' => 'material',
+  ));
+}
+
 include('data/helpers.php');
 include('components/custom-navs.php');
 include('components/functions/setup.php');
