77b3869ca3df01ef3e852b09e898a87853f99938
amimaro <amir.zahlan@gmail.com>
2020-07-04 10:01:22 -0300

    Fix comments form

--- comments.php
+++ comments.php
@@ -145,7 +145,7 @@ function customComments($comment, $args, $depth)
 					<?php printf(__('<a href="%1$s" aria-label="%2$s">Logged in as %3$s</a>. <a href="%4$s">Log out?</a>'), get_edit_user_link(), sprintf(__('Logged in as %s. Edit your profile.'), $displayName), $displayName, wp_logout_url('/')) ?>
 				</p>
 				<p class="comment-form-comment">
-					<q-input id="comment" name="comment" v-model="comment" label="<?php echo _x('Comment', 'noun'); ?>" maxlength="10000" autogrow :rules="[val => !!val || '<?php _e('Comment is required.') ?>']"></q-input>
+					<q-input id="comment" name="comment" v-model="qwpComment" label="<?php echo _x('Comment', 'noun'); ?>" maxlength="10000" autogrow :rules="[val => !!val || '<?php _e('Comment is required.') ?>']"></q-input>
 				</p>
 				<div>
 					<q-btn type="submit" label="<?php _e('Post Comment'); ?>" color="primary" class="submit"></q-btn>
@@ -166,13 +166,13 @@ function customComments($comment, $args, $depth)
 
 			<q-form ref="qwpCommentForm" id="commentform" class="comment-form q-gutter-xl" @submit="quasarwpOnSubmitComment" @reset="quasarwpOnReset">
 				<p class="comment-form-author">
-					<q-input id="author" name="author" v-model="author" label="<?php _e('Name'); ?>" maxlength="245" :rules="[val => !!val || '<?php _e('Sorry, that username is not allowed.'); ?>']"></q-input>
+					<q-input id="author" name="author" v-model="qwpAuthor" label="<?php _e('Name'); ?>" maxlength="245" :rules="[val => !!val || '<?php _e('Sorry, that username is not allowed.'); ?>']"></q-input>
 				</p>
 				<p class="comment-form-email">
-					<q-input id="email" name="email" v-model="email" label="<?php _e('Email'); ?>" maxlength="100" :rules="[val => !!val || '<?php _e('A valid email address is required.'); ?>']"></q-input>
+					<q-input id="email" name="email" v-model="qwpEmail" label="<?php _e('Email'); ?>" maxlength="100" :rules="[val => !!val || '<?php _e('A valid email address is required.'); ?>']"></q-input>
 				</p>
 				<p class="comment-form-comment">
-					<q-input id="comment" name="comment" v-model="comment" label="<?php echo _x('Comment', 'noun'); ?>" maxlength="10000" autogrow :rules="[val => !!val || '<?php _e('Comment is required.'); ?>']"></q-input>
+					<q-input id="comment" name="comment" v-model="qwpComment" label="<?php echo _x('Comment', 'noun'); ?>" maxlength="10000" autogrow :rules="[val => !!val || '<?php _e('Comment is required.'); ?>']"></q-input>
 				</p>
 				<div>
 					<q-btn type="submit" label="<?php _e('Post Comment'); ?>" color="primary" class="submit"></q-btn>
