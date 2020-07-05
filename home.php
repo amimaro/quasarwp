6f607b6787d1f0ce89ba4d2efc06d46d63efb84a
amimaro <amir.zahlan@gmail.com>
2020-07-05 19:53:07 -0300

    Add q-page to home

--- home.php
+++ home.php
@@ -12,29 +12,29 @@ include(get_template_directory() . '/components/header-functions.php');
             <?php include(get_template_directory() . '/components/quasarwp-layout.php'); ?>
 
             <q-page-container>
+                <q-page>
+                    <div class="grid-3x3-container" v-if="qwpDataHomePostLayout === 'grid3x3'">
+                        <?php
+                        if (have_posts()) : while (have_posts()) : the_post();
+                        ?>
+                                <?php include('components/post-layout/grid3x3.php'); ?>
+                        <?php
+                            endwhile;
+                        endif;
+                        ?>
+                    </div>
 
-                <div class="grid-3x3-container" v-if="qwpDataHomePostLayout === 'grid3x3'">
-                    <?php
-                    if (have_posts()) : while (have_posts()) : the_post();
-                    ?>
-                            <?php include('components/post-layout/grid3x3.php'); ?>
-                    <?php
-                        endwhile;
-                    endif;
-                    ?>
-                </div>
-
-                <div class="stacked-container" v-else>
-                    <?php
-                    if (have_posts()) : while (have_posts()) : the_post();
-                    ?>
-                            <?php include('components/post-layout/stacked.php'); ?>
-                    <?php
-                        endwhile;
-                    endif;
-                    ?>
-                </div>
-
+                    <div class="stacked-container" v-else>
+                        <?php
+                        if (have_posts()) : while (have_posts()) : the_post();
+                        ?>
+                                <?php include('components/post-layout/stacked.php'); ?>
+                        <?php
+                            endwhile;
+                        endif;
+                        ?>
+                    </div>
+                </q-page>
             </q-page-container>
         </q-layout>
     </div>
