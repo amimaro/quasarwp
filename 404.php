594a522ea79c594a5e6a55e5f1ee1f6d8c362e9f
amimaro <amir.zahlan@gmail.com>
2020-07-06 08:51:52 -0300

    Update 404 page

--- 404.php
+++ 404.php
@@ -12,9 +12,9 @@ include(get_template_directory() . '/components/header-functions.php');
       <?php include(get_template_directory() . '/components/quasarwp-layout.php'); ?>
 
       <q-page-container>
-        <q-page padding class="qwp-404">
-          <q-card>
-            <q-card-section class="q-pa-lg q-gutter-md">
+        <q-page padding class="qwp-404 q-px-xl">
+          <q-card class="q-my-md">
+            <q-card-section>
               <p class="text-h3 text-weight-light"><?php _e('Page not found'); ?> :(</p>
               <div align="right">
                 <q-btn label="<?php _e('Homepage'); ?>" color="primary" @click="quasarwpRouteTo('/')"></q-btn>
