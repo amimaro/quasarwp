98f804c586a118d7fad5248a0fc75544ef21c82e
amimaro <amir.zahlan@gmail.com>
2020-07-08 07:11:57 -0300

    Update grid3x3 comment display

--- components/post-layout/grid3x3.php
+++ components/post-layout/grid3x3.php
@@ -1,3 +1,13 @@
+<?php
+$commentsText = __('Comments', 'quasarwp');
+$commentsCount =  get_comments_number(get_post()->ID);
+$commentsCounter = sprintf(
+  /* translators: number of comments */
+  _n('%s Comment', '%s Comments', $commentsCount, 'quasarwp'),
+  $commentsCount
+);
+?>
+
 <q-card class="hover-scale qwp-home-post-card qwp-grid-3x3-item cursor-pointer" @click="quasarwpRouteTo('<?php the_permalink(); ?>')">
   <?php if (has_post_thumbnail()) : ?>
     <q-img src="<?php the_post_thumbnail_url('smallest'); ?>" :ratio="4/3" alt="" class="qwp-home-featured-img"></q-img>
@@ -18,16 +28,7 @@
   <br>
   <q-card-actions align="between" class="absolute-bottom">
     <div class="text-caption qwp-home-commentcounter">
-      <?php
-      $commentsText = __('Comments', 'quasarwp');
-      $commentsCount =  get_comments_number(get_post()->ID);
-      $commentsCounter = sprintf(
-        /* translators: number of comments */
-        _n('%s Comment', '%s Comments', $commentsCount, 'quasarwp'),
-        $commentsCount
-      );
-      echo esc_html($commentsCounter);
-      ?>
+      <?php echo esc_html($commentsCounter); ?>
     </div>
     <div class="text-caption qwp-home-postdate">
       <?php echo get_the_date(); ?>
