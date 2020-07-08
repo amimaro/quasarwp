9ecae3fb07d4c1c651e6d3c03a75babb4ccb67b6
amimaro <amir.zahlan@gmail.com>
2020-07-08 07:16:55 -0300

    Review layout phpcs:ignore menus

--- components/quasarwp-layout.php
+++ components/quasarwp-layout.php
@@ -71,28 +71,28 @@ if (has_nav_menu('right-menu')) {
 
     <q-btn id="qwp-btn-left-menu" flat dense round icon="menu" aria-label="Menu" @click="qwpLeft = !qwpLeft"></q-btn>
 
-    <?php echo $headerMenu; ?>
+    <?php echo $headerMenu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> 
 
     <q-btn id="qwp-btn-right-menu" flat dense round icon="menu" aria-label="Menu" @click="qwpRight = !qwpRight"></q-btn>
 
   </q-toolbar>
 
   <q-tabs :align="qwpDataTabsAlign">
-    <?php echo $tabMenu; ?>
+    <?php echo $tabMenu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
   </q-tabs>
 
 </q-header>
 
 <q-footer :reveal="qwpComputedFooterReveal" :elevated="qwpSelectSeparator('elevated', 'footer')" :bordered="qwpSelectSeparator('bordered', 'footer')">
   <q-toolbar>
-    <?php echo $footerMenu; ?>
+    <?php echo $footerMenu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
   </q-toolbar>
 </q-footer>
 
 <q-drawer v-model="qwpLeft" side="left" :show-if-above="qwpComputedLeftDrawerShowIfAbove" :elevated="qwpSelectSeparator('elevated', 'ldrawer')" :bordered="qwpSelectSeparator('bordered', 'ldrawer')" :overlay="qwpComputedLeftDrawerOverlay" :behavior="qwpDataLeftDrawerBehavior">
   <q-scroll-area class="fit">
     <q-list padding id="menu-list-left" class="menu-list">
-      <?php echo $leftMenu; ?>
+      <?php echo $leftMenu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
     </q-list>
   </q-scroll-area>
 </q-drawer>
@@ -100,7 +100,7 @@ if (has_nav_menu('right-menu')) {
 <q-drawer v-model="qwpRight" side="right" :show-if-above="qwpComputedRightDrawerShowIfAbove" :elevated="qwpSelectSeparator('elevated', 'rdrawer')" :bordered="qwpSelectSeparator('bordered', 'rdrawer')" :overlay="qwpComputedRightDrawerOverlay" :behavior="qwpDataRightDrawerBehavior">
   <q-scroll-area class="fit">
     <q-list padding id="menu-list-right" class="menu-list">
-      <?php echo $rightMenu; ?>
+      <?php echo $rightMenu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
     </q-list>
   </q-scroll-area>
 </q-drawer>
