f1f3bc0a22d39eed45a13d24942730e7610c97ed
amimaro <amir.zahlan@gmail.com>
2020-07-08 08:34:37 -0300

    Review customizer warnings

--- inc/customizer.php
+++ inc/customizer.php
@@ -28,64 +28,64 @@ class QuasarWP_Customize
          'title'          => __('QuasarWP', 'quasarwp')
       ));
 
-      include(get_template_directory() . '/inc/components/upload-logo.php');
-      include(get_template_directory() . '/inc/components/theme-colors.php');
-      include(get_template_directory() . '/inc/components/header.php');
-      include(get_template_directory() . '/inc/components/footer.php');
-      include(get_template_directory() . '/inc/components/left-drawer.php');
-      include(get_template_directory() . '/inc/components/right-drawer.php');
-      include(get_template_directory() . '/inc/components/tabs.php');
-      include(get_template_directory() . '/inc/components/home.php');
-      include(get_template_directory() . '/inc/components/single.php');
-      include(get_template_directory() . '/inc/components/social-icons.php');
-      include(get_template_directory() . '/inc/components/layout.php');
+      get_template_part('inc/customize', 'logo');
+      get_template_part('inc/customize', 'colors');
+      get_template_part('inc/customize', 'header');
+      get_template_part('inc/customize', 'footer');
+      get_template_part('inc/customize', 'ldrawer');
+      get_template_part('inc/customize', 'rdrawer');
+      get_template_part('inc/customize', 'tabs');
+      get_template_part('inc/customize', 'home');
+      get_template_part('inc/customize', 'single');
+      get_template_part('inc/customize', 'sharers');
+      get_template_part('inc/customize', 'layout');
    }
 
    public static function header_output()
    {
 ?>
-<!--Customizer CSS-->
-<style type="text/css">
-<?php 
-self::generate_css('.q-header', 'background-color', 'layout_header_backgroundcolor', '', ' !important');
-self::generate_css('.q-footer', 'background-color', 'layout_footer_backgroundcolor', '', ' !important');
-self::generate_css('.q-header .q-tabs', 'background-color', 'layout_tabs_backgroundcolor', '', ' !important');
-self::generate_css('.q-drawer--left', 'background-color', 'layout_ldrawer_backgroundcolor', '', ' !important');
-self::generate_css('.q-drawer--right', 'background-color', 'layout_rdrawer_backgroundcolor', '', ' !important');
-self::generate_css('#q-app', 'background-color', 'theme_background');
+      <!--Customizer CSS-->
+      <style type="text/css">
+         <?php
+         self::generate_css('.q-header', 'background-color', 'layout_header_backgroundcolor', '', ' !important');
+         self::generate_css('.q-footer', 'background-color', 'layout_footer_backgroundcolor', '', ' !important');
+         self::generate_css('.q-header .q-tabs', 'background-color', 'layout_tabs_backgroundcolor', '', ' !important');
+         self::generate_css('.q-drawer--left', 'background-color', 'layout_ldrawer_backgroundcolor', '', ' !important');
+         self::generate_css('.q-drawer--right', 'background-color', 'layout_rdrawer_backgroundcolor', '', ' !important');
+         self::generate_css('#q-app', 'background-color', 'theme_background');
 
-self::set_view('.q-header', 'display', 'layout_header_enabled');
-self::set_view('.q-footer', 'display', 'layout_footer_enabled');
-self::set_view('.q-header .q-avatar', 'display', 'layout_header_icon', 'inline-block');
-self::set_view('.q-footer .q-avatar', 'display', 'layout_footer_icon', 'inline-block');
-self::set_view('.q-header .qwp-blogname', 'display', 'layout_header_blogname', 'inline-block');
-self::set_view('.q-footer .qwp-blogname', 'display', 'layout_footer_blogname', 'inline-block');
-self::set_view('#qwp-btn-left-menu', 'display', 'layout_ldrawer_enabled', 'inline-block');
-self::set_view('#qwp-btn-right-menu', 'display', 'layout_rdrawer_enabled', 'inline-block');
-self::set_view('.q-header .q-tabs', 'display', 'layout_tabs_enabled');
-self::set_view('.qwp-home-author', 'display', 'layout_home_author');
-self::set_view('.qwp-home-excerpt', 'display', 'layout_home_excerpt');
-self::set_view('.qwp-home-postdate', 'display', 'layout_home_postdate');
-self::set_view('.qwp-home-commentcounter', 'display', 'layout_home_commentcounter');
-self::set_view('.qwp-post-author', 'display', 'layout_single_author');
-self::set_view('.qwp-post-postdate', 'display', 'layout_single_postdate');
-self::set_view('.qwp-post-commentcounter', 'display', 'layout_single_commentcounter');
-self::set_view('.qwp-post-featured-image', 'display', 'layout_single_featured_image');
-self::set_view('.qwp-post-social', 'display', 'layout_single_social');
-self::set_view('.qwp-post-comments', 'display', 'layout_single_comments');
+         self::set_view('.q-header', 'display', 'layout_header_enabled');
+         self::set_view('.q-footer', 'display', 'layout_footer_enabled');
+         self::set_view('.q-header .q-avatar', 'display', 'layout_header_icon', 'inline-block');
+         self::set_view('.q-footer .q-avatar', 'display', 'layout_footer_icon', 'inline-block');
+         self::set_view('.q-header .qwp-blogname', 'display', 'layout_header_blogname', 'inline-block');
+         self::set_view('.q-footer .qwp-blogname', 'display', 'layout_footer_blogname', 'inline-block');
+         self::set_view('#qwp-btn-left-menu', 'display', 'layout_ldrawer_enabled', 'inline-block');
+         self::set_view('#qwp-btn-right-menu', 'display', 'layout_rdrawer_enabled', 'inline-block');
+         self::set_view('.q-header .q-tabs', 'display', 'layout_tabs_enabled');
+         self::set_view('.qwp-home-author', 'display', 'layout_home_author');
+         self::set_view('.qwp-home-excerpt', 'display', 'layout_home_excerpt');
+         self::set_view('.qwp-home-postdate', 'display', 'layout_home_postdate');
+         self::set_view('.qwp-home-commentcounter', 'display', 'layout_home_commentcounter');
+         self::set_view('.qwp-post-author', 'display', 'layout_single_author');
+         self::set_view('.qwp-post-postdate', 'display', 'layout_single_postdate');
+         self::set_view('.qwp-post-commentcounter', 'display', 'layout_single_commentcounter');
+         self::set_view('.qwp-post-featured-image', 'display', 'layout_single_featured_image');
+         self::set_view('.qwp-post-social', 'display', 'layout_single_social');
+         self::set_view('.qwp-post-comments', 'display', 'layout_single_comments');
 
-self::set_view('#social-icon-whatsapp', 'display', 'social_whatsapp_enabled', 'inline-block');
-self::set_view('#social-icon-telegram', 'display', 'social_telegram_enabled', 'inline-block');
-self::set_view('#social-icon-facebook', 'display', 'social_facebook_enabled', 'inline-block');
-self::set_view('#social-icon-twitter', 'display', 'social_twitter_enabled', 'inline-block');
-self::set_view('#social-icon-e-mail', 'display', 'social_e-mail_enabled', 'inline-block');
-self::set_view('#social-icon-linkedin', 'display', 'social_linkedin_enabled', 'inline-block');
-self::set_view('#social-icon-reddit', 'display', 'social_reddit_enabled', 'inline-block');
-self::set_view('#social-icon-pinterest', 'display', 'social_pinterest_enabled', 'inline-block');
-self::set_view('#q-app', 'visibility', 'settings_loading_enabled', '', '', true);
-?>
-</style>
-<!--/Customizer CSS-->
+         self::set_view('#social-icon-whatsapp', 'display', 'social_whatsapp_enabled', 'inline-block');
+         self::set_view('#social-icon-telegram', 'display', 'social_telegram_enabled', 'inline-block');
+         self::set_view('#social-icon-facebook', 'display', 'social_facebook_enabled', 'inline-block');
+         self::set_view('#social-icon-twitter', 'display', 'social_twitter_enabled', 'inline-block');
+         self::set_view('#social-icon-e-mail', 'display', 'social_e-mail_enabled', 'inline-block');
+         self::set_view('#social-icon-linkedin', 'display', 'social_linkedin_enabled', 'inline-block');
+         self::set_view('#social-icon-reddit', 'display', 'social_reddit_enabled', 'inline-block');
+         self::set_view('#social-icon-pinterest', 'display', 'social_pinterest_enabled', 'inline-block');
+         self::set_view('#q-app', 'visibility', 'settings_loading_enabled', '', '', true);
+         ?>
+      </style>
+      <!--/Customizer CSS-->
 <?php
    }
 
@@ -102,7 +102,6 @@ self::set_view('#q-app', 'visibility', 'settings_loading_enabled', '', '', true)
 
    public static function set_view($selector, $style = 'display', $mod_name, $visible = '', $hidden = '', $reverse = false)
    {
-      $return = '';
       $value = '';
       $mod = get_theme_mod($mod_name);
       if ($style == 'display') {
@@ -115,11 +114,12 @@ self::set_view('#q-app', 'visibility', 'settings_loading_enabled', '', '', true)
       }
       if (!$reverse) $value = $mod ? $visible : $hidden;
       else $value = $mod ? $hidden : $visible;
+
       return printf(
          '%s { %s: %s; }',
-         $selector,
-         $style,
-         $value
+         esc_html($selector),
+         esc_html($style),
+         esc_html($value)
       );
    }
 
@@ -135,7 +135,7 @@ self::set_view('#q-app', 'visibility', 'settings_loading_enabled', '', '', true)
             $prefix . $mod . $postfix
          );
          if ($echo) {
-            echo $return;
+            echo esc_html($return);
          }
       }
       return $return;
